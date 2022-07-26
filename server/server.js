90d45745ea9147e56ff74fab4d3645e6a2e9e05f
Bayezid <mrbayezidhoshen@gmail.com>
2022-07-26 14:24:01 +0600

    backend product fatching  api create done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-console.log(" i Got it !!!");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const products = require("./data/products");
+
+app.get("/", (req, res) => {
+  res.send("api worked exilent 😉");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const reqProduct = products.find((p) => p._id === req.params.id);
+  res.json(reqProduct);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} Port 😍`);
+});
