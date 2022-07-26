a6cc9ce40ac87190cf646e2c1ead502b0925c6bb
Bayezid <mrbayezidhoshen@gmail.com>
2022-07-26 09:25:08 +0600

    product rating , reviews and all staff done

--- client/src/components/Product/ProductItem.js
+++ client/src/components/Product/ProductItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import ProductRating from "./ProductRating";
 
 const ProductItem = ({ product }) => {
   return (
@@ -8,23 +9,25 @@ const ProductItem = ({ product }) => {
         <a href={`product/${product._id}`}>
           <Card.Img src={product.image} variant="top" />
         </a>
+
+        <Card.Body>
+          <a href={`product/${product._id}`}>
+            <Card.Title as="div">
+              {" "}
+              <strong> {product.name} </strong>{" "}
+            </Card.Title>
+          </a>
+
+          <Card.Text as="div">
+            <ProductRating
+              value={product.rating}
+              text={`${product.numReviews} Reviews`}
+            />
+          </Card.Text>
+
+          <Card.Text as="h3"> ${product.price} </Card.Text>
+        </Card.Body>
       </Card>
-      <Card.Body>
-        <a href={`product/${product._id}`}>
-          <Card.Title as="div">
-            {" "}
-            <strong> {product.name} </strong>{" "}
-          </Card.Title>
-        </a>
-
-        <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
-        </Card.Text>
-
-        <Card.Text as="h3"> ${product.price} </Card.Text>
-      </Card.Body>
     </div>
   );
 };
