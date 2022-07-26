d6b3a5e08914692c187df003287db8075a1f4da3
Bayezid <mrbayezidhoshen@gmail.com>
2022-07-26 15:55:13 +0600

    fetch date from backend with axios

--- client/src/screen/HomeScreen.js
+++ client/src/screen/HomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-import products from "../products";
 import ProductItem from "../components/Product/ProductItem";
+import axios from "axios";
 
 export const HomeScreen = () => {
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+    fetchProducts();
+  });
+
   return (
     <div>
       <h1> Products </h1>
       <Row>
-        {products.map((product) => (
+        {Products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <ProductItem product={product} />
           </Col>
